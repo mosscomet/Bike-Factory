f1d02567c11ee0ceb41b7896dbd9b066c559549d
MossComet <mosscomet@proton.me>
2026-07-29 00:29:10 -0400

    Factory Upgrades + Code Improvements
    Made upgrades work and improved code with getInt and getFloat functions.

--- app.py
+++ app.py
@@ -9,10 +9,18 @@ processes = {
     "Packaging":            {"processing time":5.0, "downtime":0.0, "defect rate":0.005, "daily":0.0, "yield":0.0, "utilization": 0.0, "idle":0.0},
 }
 
-workingMinutes = 480
-sellingPrice = 160
-customerDemand = 55
-currentBudget = 30000
+factoryUpgrades = {
+    "Faster Wheel Equipment": {"bought":False, "cost":20000, "description": "Wheel time -4 min"},
+    "Additional Brake Workstation": {"bought":False, "cost":15000, "description": "Brake capacity doubles"},
+    "Better Inspection equipment": {"bought":False, "cost":12000, "description": "Inspection time -3 min"},
+    "Worker Training": {"bought":False, "cost":8000, "description": "Defects reduced by 40%"},
+    "Conveyor Improvement": {"bought":False, "cost":10000, "description": "Reduce each station processing time by 0.5 min"}
+}
+
+workingMinutes = 480.0
+sellingPrice = 160.0
+customerDemand = 55.0
+currentBudget = 30000.0
 systemCapacity = 0.0
 systemYield = 0.0
 bottleneck = ""
@@ -100,6 +108,14 @@ def getInt(prompt):
         except ValueError:
             print("Please enter a number.")
 
+def getFloat(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Please enter a number.")
+
+
 def createMenu():
     updateNumbers()
     while True:
@@ -120,19 +136,19 @@ def createMenu():
         print("   6. Exit")
 
 
-        userChoice = input("Select an option: ")
+        userChoice = getInt("Select an option: ")
         match userChoice:
-            case '1':
+            case 1:
                 viewFactory()
-            case '2':
+            case 2:
                 modifyFactory()
-            case '3':
+            case 3:
                 investImprove()
-            case '4':
+            case 4:
                 runSimulation()
-            case '5': 
+            case 5: 
                 ViewFinancials()
-            case '6': 
+            case 6: 
                 sys.exit()
             case _:
                 print("unknown input")
@@ -155,7 +171,6 @@ def viewFactory():
                 break
             else:
                 print("Please choose a valid option.")
-
         if userChoice == len(processes) + 2:
             return()
         if userChoice == len(processes) + 1:
@@ -195,22 +210,16 @@ def modifyFactory():
         print(f"   {len(processes) + 2}. Return")
 
         while True:
-            userChoice = input("Choose an option: ")
+            userChoice = getInt("Choose an option: ")
 
-            try:
-                choice = int(userChoice)
-            except:
-                print("Please enter a number.")
-                continue
-
-            if 1 <= choice <= len(processes) + 2:
+            if 1 <= userChoice <= len(processes) + 2:
                 break
             else:
                 print("Please choose a valid option.")
 
-        if choice == len(processes) + 2:
+        if userChoice == len(processes) + 2:
             return()
-        if choice == len(processes) + 1:
+        if userChoice == len(processes) + 1:
             print(f"Factory Stats:")
             print(f"   1. Working Minutes: {workingMinutes}")
             print(f"   2. Selling Price: {sellingPrice}")
@@ -219,52 +228,113 @@ def modifyFactory():
             print(f"   5. Profit Per Bike: {profitPerBike}")
             print(f"   6. Return")
             while True:
-                userChoice = input("Choose an option: ")
-                    
-                try:
-                    choice = int(userChoice)
-                except:
-                    print("Please enter a number.")
-                    continue
+                userChoice = getInt("Choose an option: ")
 
-                match choice:
+                match userChoice:
                     case 1:
-                        workingMinutes = getInt("Choose new value: ")
+                        workingMinutes = getFloat("Choose new value: ")
                     case 2:
-                        sellingPrice = getInt("Choose new value: ")
+                        sellingPrice = getFloat("Choose new value: ")
                     case 3:
-                        customerDemand = getInt("Choose new value: ")
+                        customerDemand = getFloat("Choose new value: ")
                     case 4:
-                        currentBudget = getInt("Choose new value: ")
+                        currentBudget = getFloat("Choose new value: ")
                     case 5:
-                        profitPerBike = getInt("Choose new value: ")
+                        profitPerBike = getFloat("Choose new value: ")
                     case 6:
                         return
                 updateNumbers()   
         else:
             updateNumbers()
             listOfProcesses = list(processes.keys())
-            selectedProcess = listOfProcesses[choice - 2]
+            selectedProcess = listOfProcesses[userChoice - 1]
             print(f"{selectedProcess}:")
-            print(f"   Processing Time: {processes[selectedProcess]['processing time']}")
-            print(f"   Downtime: {processes[selectedProcess]['downtime']}")
-            print(f"   Defect Rate: {processes[selectedProcess]['defect rate']}")
-            print(f"   Daily Capacity: {processes[selectedProcess]['daily']}")
-            print(f"   Process Yield: {processes[selectedProcess]['yield']}")
-            print(f"   Utilization: {processes[selectedProcess]['utilization']}")
-            print(f"   Idle Capacity: {processes[selectedProcess]['idle']}")
+            print(f"   1. Processing Time: {processes[selectedProcess]['processing time']}")
+            print(f"   2. Downtime: {processes[selectedProcess]['downtime']}")
+            print(f"   3. Defect Rate: {processes[selectedProcess]['defect rate']}")
+            print(f"   4. Return")
+            while True:
+                userChoice = getInt("Choose an option: ")
+
+                match userChoice:
+                    case 1:
+                        processes[selectedProcess]['processing time'] = getFloat("Choose new value: ")
+                    case 2:
+                        processes[selectedProcess]['downtime'] = getFloat("Choose new value: ")
+                    case 3:
+                        processes[selectedProcess]['defect rate'] = getFloat("Choose new value: ")
+                    case 4:
+                        return
+                updateNumbers()
+
 
 def investImprove():
-    print("investImprove")
-    createMenu()
+    global currentBudget
+    while True:
+        print("===============")
+        print("Upgrade Factory")
+        print("===============")
+        print(f"Budget Available: ${currentBudget}")
+        for i, upgrade in enumerate(factoryUpgrades, start=1):
+            print(f"   {i}. ${factoryUpgrades[upgrade]['cost']} - {upgrade} - {factoryUpgrades[upgrade]['bought']}")
+        print(f"   {len(factoryUpgrades) + 1}. Return")
+
+        while True:
+            userChoice = getInt("Choose an option: ")
+
+            if 1 <= userChoice <= len(factoryUpgrades) + 1:
+                break
+            else:
+                print("Please choose a valid option.")
+
+        if userChoice == len(factoryUpgrades) + 1:
+            return
+        else:
+            listOfUpgrades = list(factoryUpgrades.keys())
+            selectedUpgrade = listOfUpgrades[userChoice - 1]
+            print(f"{selectedUpgrade} selected!")
+            print(f"   {factoryUpgrades[selectedUpgrade]['description']}")
+            print(f"   1. Buy")
+            print(f"   2. Do not")
+            userChoice = getInt("Purchase? ")
+            match userChoice:
+                case 1:
+                    if factoryUpgrades[selectedUpgrade]["cost"] <= currentBudget and factoryUpgrades[selectedUpgrade]["bought"] == False:
+                        currentBudget -= factoryUpgrades[selectedUpgrade]["cost"]
+                        factoryUpgrades[selectedUpgrade]["bought"] = True
+                        purchaseUpgrade(selectedUpgrade)
+                case 2:
+                    return
+            updateNumbers()
+
+def purchaseUpgrade(selectedUpgrade):
+    print(selectedUpgrade)
+    if selectedUpgrade == "Faster Wheel Equipment":
+        if processes["Wheel Installation"]["processing time"] > 4:
+            processes["Wheel Installation"]["processing time"] -= 4
+            updateNumbers()
+    elif selectedUpgrade == "Additional Brake Workstation":
+        processes[selectedUpgrade]["processing time"] /= 2
+        updateNumbers()
+    elif selectedUpgrade == "Better Inspection equipment":
+        if processes["Brake Installation"]["processing time"] > 3:
+            processes["Brake Installation"]["processing time"] -= 3
+            updateNumbers()
+    elif selectedUpgrade == "Worker Training":
+        for process in processes:
+            processes[process]["defect rate"] *= 0.6
+            updateNumbers()
+    elif selectedUpgrade == "Conveyor Improvement":
+        for process in processes:
+            if processes[process]["processing time"] > 0.5:
+                processes[process]["processing time"] -= 0.5
+        updateNumbers()
 
 def runSimulation():
     print("runSimulation")
-    createMenu()
 
 def ViewFinancials():
     print("ViewFinancials")
-    createMenu()
 
 
 
